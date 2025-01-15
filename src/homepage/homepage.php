ce228d6168f045d13c09571ac32d79e7b8e2b974
jwnaina <yanainavieira@gmail.com>
2025-01-15 17:29:16 -0300

    add nav

---
+++ src/homepage/homepage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afternoon Tea</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul><a href="#">Books</a>
+                <a href="#">Movies</a>
+                <a href="#">Shows</a>
+                <a href="#">Games</a>
+            </ul>
+            <form class="search-bar">
+                <input type="text" placeholder="Search reviews" name="search">
+                <button type="submit">Search</button>
+            </form>
+            <a href="#">User</a>
+        </nav>
+    </header>
+
+
+</body>
+</html>
