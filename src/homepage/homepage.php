44bbb959b9300b2a4a111fd71cf94317619d321f
jwnaina <yanainavieira@gmail.com>
2025-01-16 12:23:05 -0300

    add nav bar

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -4,23 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Afternoon Tea</title>
+    <link href="../styles/global.css" rel="stylesheet">
 </head>
 <body>
     <header>
         <nav>
-            <ul><a href="#">Books</a>
-                <a href="#">Movies</a>
-                <a href="#">Shows</a>
-                <a href="#">Games</a>
+            <div class="left-nav">
+            <a href="#"><img src="../assets/images/Logo.png"></a>
+            <ul>
+                <a href="#">books</a>
+                <a href="#">movies</a>
+                <a href="#">shows</a>
+                <a href="#">games</a>
             </ul>
+            </div>
+
+            <div class="right-nav">
             <form class="search-bar">
                 <input type="text" placeholder="Search reviews" name="search">
-                <button type="submit">Search</button>
+                <button type="submit"><img src="../assets/images/search.png"></button>
             </form>
-            <a href="#">User</a>
+            <a href="#"><img src="../assets/images/userprofile.png"></a>
+</div>
         </nav>
     </header>
 
+    <section id="lastreviewbanner">
+        <div id="covercontainerbanner">
+            <div id="titlecontainerbanner"> 
+            </div>
+            <button id="seereviewbanner">see review</button>
+        </div>
+    </section>
 
 </body>
 </html>
