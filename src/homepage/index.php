6a4f742f4223cca8f167d7fd64ae14137863f552
jwnaina <yanainavieira@gmail.com>
2025-01-16 17:22:38 -0300

    add nav global

---
+++ src/homepage/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afternoon Tea</title>
+    <link href="../styles/global.css" rel="stylesheet">
+</head>
+<body>
+    <?php 
+    include('../global/nav.html'); 
+    ?>
+    <section id="lastreviewbanner">
+        <div id="covercontainerbanner">
+            <div id="titlecontainerbanner"> 
+            </div>
+            <button id="seereviewbanner">see review</button>
+        </div>
+    </section>
+
+</body>
+</html>
